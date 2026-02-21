7d2fe76215865947f76b3d26e49f07193944c2a8
JFarnsworth2025 <jfarnsworthpersonal@gmail.com>
2026-02-20 20:48:19 -0800

    Initial Spring Boot setup with Java 17 and PostgreSQL

---
+++ src/test/java/com/jacob/flowtrack/FlowtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.flowtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
